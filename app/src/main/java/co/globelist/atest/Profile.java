62a855b43f2f8601fbd9b35692b587525fb2ecab
Ádám Gólya <adam.stork@gmail.com>
2015-12-27 18:42:20 +0100

    change action text
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- app/src/main/java/co/globelist/atest/Profile.java
+++ app/src/main/java/co/globelist/atest/Profile.java
@@ -31,7 +31,7 @@ public class Profile extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Edit profile", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
